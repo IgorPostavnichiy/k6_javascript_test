a7585843fe536da3062e3ddf42bc66ab1b37ed3f
IgorPostavnichiy <igorpostavnichiy@gmail.com>
2023-08-03 15:18:16 +0300

    add changes file

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
 import { Counter } from 'k6/metrics';
+import { sleep } from 'k6';
 
 let numIterations = 100;
+let appCounter = 0;
+let clickCounter = 0;
 
 export let options = {
   vus: 1,
-  iterations: 1,
-  duration: '10m',
+  iterations: numIterations,
+  duration: '20m',
 };
 
 export let successfulLoads = new Counter('Successful Loads');
 
 export default function () {
   const baseAppId = 'com.sinyee.babybus.world';
-  let appsVisited = 0;
-
-  crawlSimilarApps(baseAppId, appsVisited);
+  crawlSimilarApps(baseAppId);
 }
 
-function crawlSimilarApps(packageName, appsVisited) {
-  if (appsVisited >= numIterations) {
-    console.log(`Reached ${numIterations} apps. Stopping...`);
-    return;
-  }
-
+function crawlSimilarApps(packageName) {
   const BASE_URL = `https://play.google.com/store/apps/details?id=${packageName}`;
+  sleep(Math.random() * 5 + 5);
 
-  // Используем ожидание между запросами с помощью sleep
-  sleep(Math.random() * 5 + 5); // Ожидание от 5 до 10 секунд
+  const response = http.get(BASE_URL, { timeout: '30s' });
+  successfulLoads.add(1);
 
-  const response = http.get(BASE_URL, { timeout: '30s' }); // Увеличиваем время ожидания для запроса
-
-  check(response, {
-    'is status 200': (r) => r.status === 200,
-  });
+  appCounter++;
+  console.log(`Checked app: ${packageName}, Total apps checked: ${appCounter}`);
 
   const similarApps = extractSimilarApps(response.body);
 
-  if (similarApps.length === 0) {
-    console.log(`No similar apps found for ${packageName}`);
+  if (similarApps.length === 0 || clickCounter >= 100) {
+    console.log('Finished checking apps.');
     return;
   }
 
   for (const appPackage of similarApps) {
-    if (appPackage !== packageName) {
-      appsVisited++;
-      crawlSimilarApps(appPackage, appsVisited);
-
-      if (appsVisited >= numIterations) {
-        console.log(`Reached ${numIterations} apps. Stopping...`);
-        return;
-      }
+    if (appPackage !== packageName && clickCounter < 100) {
+      clickCounter++;
+      crawlSimilarApps(appPackage);
     }
   }
-
-  successfulLoads.add(1);
 }
 
 function extractSimilarApps(responseBody) {
@@ -66,10 +52,8 @@ function extractSimilarApps(responseBody) {
   while ((match = regex.exec(responseBody)) !== null) {
     if (match[1]) {
       similarApps.push(match[1]);
-      console.log(`Found similar app: ${match[1]}`);
     }
   }
 
   return similarApps;
 }
-
